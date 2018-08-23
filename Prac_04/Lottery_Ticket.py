d4fc51556e51878a24036c204cfbbdddbfac43a8
jc388991 <james.yalo@my.jcu.edu.au>
2018-08-23 16:41:19 +1000

    print formate was changed

--- Prac_04/Lottery_Ticket.py
+++ Prac_04/Lottery_Ticket.py
@@ -7,8 +7,8 @@ MAXIMUM = 45
 
 def main():
     number_quick_pick = int(input("How many numbers you picking?"))
-    while number_quick_pick < 0:
-        print("Not making any sense")
+    while number_quick_pick <= 0:
+        print("Number must be greater than zero")
         number_quick_pick = int(input("How many numbers you picking"))
     for i in range(number_quick_pick):
         quick_pick = []
@@ -18,7 +18,9 @@ def main():
                 number = random.randint(MINIMUM, MAXIMUM)
             quick_pick.append(number)
         quick_pick.sort()
-        print(quick_pick)
+        for j in quick_pick:
+            print("{:4}".format(j), end='')
+        print()
 
 
 main()
