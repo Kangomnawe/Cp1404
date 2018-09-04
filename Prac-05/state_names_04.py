2256a5c8d9f37febbd9531d47ee7513aa4196cd9
jc388991 <james.yalo@my.jcu.edu.au>
2018-09-04 15:17:35 +1000

    Practical number 5

---
+++ Prac-05/state_names_04.py
@@ -0,0 +1,30 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+
+
+STATE_NAMES_dict = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+for i in state:
+    if state in state_list:
+        STATE_NAMES[state] += 1
+    else:
+        STATE_NAMES_dict[state] = STATE_NAMES_dict.get(state, 0) + 1
+
+# print(STATE_NAMES)
+
+# state = input("Enter short state: ").upper()
+# while state != "":
+#     for state in STATE_NAMES:
+#         print(STATE_NAMES)
+# else:
+#     print("Invalid state")
+
+# if state in STATE_NAMES:
+#     print(state, "is", STATE_NAMES[state])
+# else:
+#     print("Invalid short state")
+# state = input("Enter short state: ").upper()
---
+++ Prac-05/word_occurrences.py
@@ -0,0 +1,15 @@
+""" Out put
+Text: this is a collection of words of nice words this is a fun thing it is
+a : 2
+collection : 1
+fun : 1
+is : 3
+it : 1
+nice : 1
+of : 2
+thing : 1
+this : 2
+words : 2
+"""
+
+text = "this is a collection of words of nice words this is a fun thing it is"
