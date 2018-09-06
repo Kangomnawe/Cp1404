9be16f869066f39e955922654431f938d3d6800a
jc388991 <james.yalo@my.jcu.edu.au>
2018-09-06 17:23:33 +1000

    word count

--- Prac-05/word_occurrences.py
+++ Prac-05/word_occurrences.py
@@ -12,29 +12,44 @@ this : 2
 words : 2
 """
 
+"""
+Text: this is a collection of words of nice words this is a fun thing it is
+"""
+string = input("Enter string:")
+word = input("Enter word:")
+a = []
+count = 0
+a = string.split(" ")
+for i in range(0, len(a)):
+    if (word == a[i]):
+        count = count + 1
+print("Count of the word is:")
+print(count)
 
-text = {1: 'this', 2: 'is', 3: 'a', 4:'collection', 5: 'of', 6: 'words', 7: 'of', 8: 'nice', 9: 'words', 11: 'this',
-        12: 'is', 13: 'a', 14: ' fun', 15: 'thing', 16: 'it', 17: 'is'}
 
-word = input("Enter you any word: ")
-if len(word) < 1:
-    word = 'text'
+
+
+
+
+# word = input("Enter you any word: ")
+# if len(word) < 1:
+#     word = 'text'
+# # pass
+# # text = open(word)
 # pass
-# text = open(word)
-pass
-# print(text)
-
-di = dict()
-
-for lin in text:
-    lin = lin.rstrip()
-    # print(lin)
-    wds = lin.split()
-    # print(wds)
-    for w in wds:
-        if w in di:
-            di[w] = di[w] + 1
-        else:
-            di[w] = 1
-            print("count")
-        print(w, di[w])
+# # print(text)
+#
+# di = dict()
+#
+# for lin in text:
+#     lin = lin.rstrip()
+#     # print(lin)
+#     wds = lin.split()
+#     # print(wds)
+#     for w in wds:
+#         if w in di:
+#             di[w] = di[w] + 1
+#         else:
+#             di[w] = 1
+#             print("count")
+#         print(w, di[w])
