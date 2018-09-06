ada9fe1f4808847d5f961fff267cd89f889e5b60
jc388991 <james.yalo@my.jcu.edu.au>
2018-09-06 15:51:15 +1000

    list

---
+++ Prac-04/warm_up.py
@@ -0,0 +1,14 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+
+ numbers[0] =3      # have 3
+ numbers[-1] =2     # have 2
+ numbers[3] =1      # have 1
+ numbers[:-1] = [4, 1, 4, 1, 5, 9]      # will have list of numbers [4, 1, 4, 1, 5, 9]
+ numbers[3:4] =[1]      # have 1
+ 5 in numbers = True    # True
+ 7 in numbers =False    # False
+ "3" in numbers =False  # False
+ numbers + [6, 5, 3] =[3, 1, 4, 5, 9]   # will have list of [3, 1, 4, 5, 9]
+
+numbers [numbers]+[10]
