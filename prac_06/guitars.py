37fef209c3cf43ecda0e8cc6766f57b4cb18b8db
jc388991 <james.yalo@my.jcu.edu.au>
2018-09-20 12:35:04 +1000

    box layout

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -4,12 +4,17 @@ from prac_06.guitar_class import Guitar
 def main():
     print("My Guitars!")
     guitars = []
-    # for guitar in guitars:
-    #     guitars.append(guitar)
-
-    name = input("Name: ")
-    while name != "":
-
+    guitar_model =input("namer:")
+    while guitar_model != "":
+        name = input("Name:")
+        year = input("Year:")
+        cost = input("Cost")
+        if name == "":
+            break
+        # else:
+        #     guitars.append(name)
+        #     guitars = sorted(guitars)
+        #     # print(guitars)
 
     # where we do the things we want to do in the loop
 
@@ -18,10 +23,10 @@ def main():
         # add guitar object to list of guitars
         # print message saying guitar has been added
 
-        name = input("Name: ")
+        # name = input("Name: ")
 
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     # print(guitars)
     # add code to display a list of the guitars
